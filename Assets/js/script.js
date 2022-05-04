594f057c9f17cbe0e32c4900f71ee2277d9b8b9d
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-03 19:15:51 -0700

    add score to localstorage at end of quiz

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
 // array of objects with all questions and answers
 const Questions = [
   {
-    id: 0,
+    id: 1,
     question: "Question one?",
     choices: [
       { text: "Answer 1-1", isCorrect: false },
@@ -12,7 +12,7 @@ const Questions = [
     ],
   },
   {
-    id: 1,
+    id: 2,
     question: "Question two?",
     choices: [
       { text: "Answer 2-1", isCorrect: false },
@@ -23,8 +23,8 @@ const Questions = [
     ],
   },
   {
-    id: 2,
-    questions: "Question three?",
+    id: 3,
+    question: "Question three?",
     choices: [
       { text: "Answer 3-1", isCorrect: true },
       { text: "Answer 3-2", isCorrect: false },
@@ -36,49 +36,84 @@ const Questions = [
 ];
 
 const startSection = document.getElementById("start");
-const startQuizBtn = document.getElementById("quiz-start-btn");
-
 const quizSection = document.getElementById("quiz");
-const next = document.getElementById("next");
-
 const finishSection = document.getElementById("finish");
 const scoresSection = document.getElementById("scores");
 
+const startQuizBtn = document.getElementById("quiz-start-btn");
+const nextBtn = document.getElementById("nextBtn");
+
+const question = document.getElementById("question");
+const choices = document.getElementById("choices");
+// const result = document.getElementById("result");
+const correctTxt = document.getElementById("correct");
+const wrongTxt = document.getElementById("wrong");
+
+const totalScoreTxt = document.getElementById("quiz-score");
+
+// set score to 0 and current question to 0
+let score = 0;
+let currentQuestion = 0;
+
+// get total number of questions
+const questionCount = Object.keys(Questions).length;
+
 startQuizBtn.addEventListener("click", () => {
   startSection.style.display = "none";
-  quiz(0);
+  quiz(currentQuestion);
 });
 
-let score = 0;
+nextBtn.addEventListener("click", () => {
+  nextBtn.style.display = "none";
+  correctTxt.style.display = "none";
+  wrongTxt.style.display = "none";
+  currentQuestion++;
+  quiz(currentQuestion);
+});
 
 function quiz(id) {
-  const question = document.getElementById("question");
-  const correct = document.getElementById("correct");
-  const wrong = document.getElementById("wrong");
+  if (id === questionCount) {
+    quizSection.style.display = "none";
 
-  // insert question
-  question.innerText = Questions[id].question;
+    localStorage.setItem("totalScore", score);
+    totalScoreTxt.innerHTML = localStorage.getItem("totalScore");
 
-  // grab all choice li's on page
-  const choiceItems = document.querySelectorAll(".c-item");
+    finishSection.style.display = "block";
+  } else {
+    // show current question
+    console.log("current question: " + currentQuestion);
+    console.log("current score: " + score);
 
-  // attach text and isCorrect boolean to all li's
-  for (const [i, c] of choiceItems.entries()) {
-    c.innerText = Questions[id].choices[i].text;
+    // insert question text
+    question.innerText = Questions[id].question;
 
-    let isCorrect = Questions[id].choices[i].isCorrect;
+    // grab all choice buttons on page
+    const choiceItems = document.querySelectorAll(".c-item");
 
-    c.addEventListener("click", function () {
-      if (isCorrect) {
-        score++;
-        correct.style.display = "block";
-      } else {
-        wrong.style.display = "block";
-      }
-      next.style.display = "block";
-    });
+    // attach text and isCorrect boolean to all buttons
+    for (const [i, c] of choiceItems.entries()) {
+      // insert choice text in buttons
+      c.innerText = Questions[id].choices[i].text;
 
-    //c.disabled = true;
+      // set true or false
+      let isCorrect = Questions[id].choices[i].isCorrect;
+
+      // when choice button is clicked
+      c.addEventListener("click", function () {
+        if (isCorrect) {
+          score++;
+          correctTxt.style.display = "block";
+          wrongTxt.style.display = "none";
+          console.log("correct!");
+        } else {
+          correctTxt.style.display = "none";
+          wrongTxt.style.display = "block";
+          console.log("wrong...you get zero points"); 
+        }
+        //choices.disabled = true;
+        nextBtn.style.display = "block";
+      });
+    }
   }
 
   // display quiz
