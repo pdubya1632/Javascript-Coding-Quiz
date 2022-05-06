b5d3fe42c90ca7fce4243ebfe0c3dd88b0747234
Patrick Richardson Wright <wrightmight@gmail.com>
2022-05-05 23:31:11 -0700

    finish timer and add high scores to final page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,33 +1,43 @@
 // array of objects with all questions and answers
-const Questions = [
+const QuizContent = [
   {
     id: 1,
-    question: "Question one?",
+    question: "Question one",
     choices: [
       { text: "Answer 1-1", isCorrect: false },
       { text: "Answer 1-2", isCorrect: true },
       { text: "Answer 1-3", isCorrect: false },
-      { text: "Answer 1-4", isCorrect: false }
+      { text: "Answer 1-4", isCorrect: false },
     ],
   },
   {
     id: 2,
-    question: "Question two?",
+    question: "Question two",
     choices: [
       { text: "Answer 2-1", isCorrect: false },
       { text: "Answer 2-2", isCorrect: false },
       { text: "Answer 2-3", isCorrect: false },
-      { text: "Answer 2-4", isCorrect: true }
+      { text: "Answer 2-4", isCorrect: true },
     ],
   },
   {
     id: 3,
-    question: "Question three?",
+    question: "Question three",
     choices: [
       { text: "Answer 3-1", isCorrect: true },
       { text: "Answer 3-2", isCorrect: false },
       { text: "Answer 3-3", isCorrect: false },
-      { text: "Answer 3-4", isCorrect: false }
+      { text: "Answer 3-4", isCorrect: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "Question four",
+    choices: [
+      { text: "Answer 4-1", isCorrect: false },
+      { text: "Answer 4-2", isCorrect: false },
+      { text: "Answer 4-3", isCorrect: true },
+      { text: "Answer 4-4", isCorrect: false },
     ],
   },
 ];
@@ -36,6 +46,7 @@ const startSection = document.getElementById("start");
 const quizSection = document.getElementById("quiz");
 const finishSection = document.getElementById("finish");
 const scoresSection = document.getElementById("scores");
+const startOverModal = document.getElementById("start-over-modal");
 
 const question = document.getElementById("question");
 const choices = document.getElementById("choices");
@@ -44,143 +55,183 @@ const correctTxt = document.getElementById("correct");
 const wrongTxt = document.getElementById("wrong");
 
 const startQuizBtn = document.getElementById("quiz-start-btn");
-const choiceBtns = choices.getElementsByTagName('*');
-const nextBtn = document.getElementById("nextBtn");
+const choiceBtns = choices.getElementsByTagName("*");
+const nextBtn = document.getElementById("next-btn");
 const submitBtn = document.getElementById("submit-btn");
+const tryAgainBtn = document.getElementById("tryAgain-btn");
+const clearBtn = document.getElementById("clear-btn");
 
+const viewScores = document.getElementById("view-scores");
 const initialsTxt = document.getElementById("initials");
 const totalScoreTxt = document.getElementById("quiz-score");
+const timer = document.getElementById("timer");
 
-// set score to 0, current question to 0, and set total number of questions
 let score = 0;
 let currentQuestion = 0;
+let timeLeft = 30;
+const questionCount = QuizContent.length;
+let isQuizActive = true;
 
-// get total number of questions
-const questionCount = Questions.length;
-const startTime = 30;
+// add Time Left: upon page load
+document.getElementById("timer").textContent = timeLeft;
 
-submitBtn.addEventListener("click", () => {
+// TIMER
+function countdownTimer() {
+  let counter = timeLeft;
 
-  // log initials and score to browser
-  localStorage.setItem(initialsTxt.value, score);
-
-  finishSection.display = "none";
-  scoresSection.display = "block";
-  timeLeft.innerHTML = startTime;
-});
-
-startQuizBtn.addEventListener("click", () => {
-  startSection.style.display = "none";
-  
-  // set counter start to be text that's within timer span
-  let counter = startTime; 
-
-  // start timer countdown function
-  const interval = setInterval(startTimer, 1000);
-    
-  function startTimer() {
-    counter--;
-  
-    if( counter >= 0 ) {
-      timeLeft = document.getElementById("timer");
-      timeLeft.innerHTML = counter;
+  const timerInterval = setInterval(function () {
+    if (counter > 0 && isQuizActive == true) {
+      counter--;
+      timer.innerHTML = counter;
     }
-    else if( counter === 0 && currentQuestion <= questionCount ) {
-      window.alert("time is up! start over");
-      resetTimer();
-    } 
-  };
+    else if (isQuizActive == false) {
+      clearInterval(timerInterval);
+      timer.innerText = timeLeft;
+    }
+    else {
+      clearInterval(timerInterval);
+      if (!alert("You're out of time! Please try again.")) {
+        location.reload();
+      }
+    }
+  }, 1000);
+}
 
-  function resetTimer() {
-    clearInterval(interval);
-    document.getElementById("timer").innerText = startTime;
-  };
+// SHOW/HIDE SECTIONS & BUTTONS
+const displayToggle = {
+  show: function (id) {
+    id.style.display = "block";
+  },
+  hide: function (id) {
+    id.style.display = "none";
+  },
+  inline: function (id) {
+    id.style.display = "inline";
+  },
+};
+
+// START QUIZ
+startQuizBtn.addEventListener("click", () => {
+  countdownTimer();
+
+  displayToggle.hide(startSection);
+  displayToggle.show(quizSection);
 
   quiz(currentQuestion);
 });
 
+// NEXT BUTTON CLICK
 nextBtn.addEventListener("click", () => {
-  nextBtn.style.display = "none";
-  result.style.display = "none";
-  correctTxt.style.display = "none";
-  wrongTxt.style.display = "none";
-  
+  displayToggle.hide(nextBtn);
+  displayToggle.hide(result);
+  displayToggle.hide(correctTxt);
+  displayToggle.hide(wrongTxt);
+
   // enable choice buttons
   for (let node of choiceBtns) {
     node.disabled = false;
   }
 
   currentQuestion++;
-  quiz(currentQuestion);
+
+  if (currentQuestion < questionCount) {
+    quiz(currentQuestion);
+  } else {
+    totalScoreTxt.innerHTML = score;
+
+    isQuizActive = false;
+    currentQuestion = 0;
+
+    displayToggle.show(finishSection);
+    displayToggle.hide(quizSection);
+  }
 });
 
 // SUBMIT BUTTON CLICK
 submitBtn.addEventListener("click", () => {
-
+  
   // log initials and score to browser
   localStorage.setItem(initialsTxt.value, score);
 
-  finishSection.style.display = "none";
-  timeLeft.innerHTML = startTime;
+  // reset currentQuestion and clear initials field
+  currentQuestion = 0;
+  initialsTxt.value = "";
 
-  const scoreList = document.getElementById("score-list");
-  scoresSection.style.display = "block";
+  displayToggle.hide(finishSection);
+  displayToggle.show(scoresSection);
+
+  // add all scores to ul
+  for (let i = 0; i < localStorage.length; i++) {
+    keys = Object.keys(localStorage);
+    document.getElementById("score-list").innerHTML += "<li>" + localStorage.getItem(keys[i]) + "</li>";
+  }
 
 });
 
+// TRY AGAIN
+tryAgainBtn.addEventListener("click", () => {
+  displayToggle.hide(scoresSection);
+  displayToggle.show(startSection);
+
+  isQuizActive = true;
+
+  // show View High Scores link
+  if (localStorage.length != 0 && viewScores.style.display === "none") {
+    displayToggle.inline(viewScores);
+  }
+});
+
+// CLEAR HIGH SCORES
+clearBtn.addEventListener("click", () => {
+  localStorage.clear();
+  displayToggle.hide(viewScores);
+});
+
+//--------------QUIZ FUNCTION---------------//
+
 function quiz(id) {
-  if (id === questionCount) {
-    totalScoreTxt.innerHTML = score;
+  // insert question text
+  question.innerText = QuizContent[id].question;
 
-    clearInterval();
+  // grab all choice buttons on page
+  const choiceItems = document.querySelectorAll(".choice");
 
-    finishSection.style.display = "block";
-    quizSection.style.display = "none";
-  } else {
-    // show current question
-    console.log("current question: " + currentQuestion);
-    console.log("current score: " + score);
+  // attach text and isCorrect boolean to all buttons
+  for (const [i, c] of choiceItems.entries()) {
+    // reset choice border color
+    c.style.border = "1px solid purple";
 
-    // display quiz
-    quizSection.style.display = "block";
+    // insert choice text in buttons
+    c.innerText = QuizContent[id].choices[i].text;
 
-    // insert question text
-    question.innerText = Questions[id].question;
+    // set true or false
+    let isCorrect = QuizContent[id].choices[i].isCorrect;
 
-    // grab all choice buttons on page
-    const choiceItems = document.querySelectorAll(".choice");
+    // when choice button is clicked
+    c.addEventListener("click", function () {
+      // give selected choice a white border
+      c.style.border = "1px solid #fff";
 
-    // attach text and isCorrect boolean to all buttons
-    for (const [i, c] of choiceItems.entries()) {
-      // insert choice text in buttons
-      c.innerText = Questions[id].choices[i].text;
+      if (isCorrect) {
+        score++;
+        displayToggle.show(result);
+        displayToggle.show(correctTxt);
+        displayToggle.hide(wrongTxt);
+        console.log("correct! you get one point");
+      } else {
+        displayToggle.show(result);
+        displayToggle.hide(correctTxt);
+        displayToggle.show(wrongTxt);
+        console.log("wrong...you get zero points");
+      }
 
-      // set true or false
-      let isCorrect = Questions[id].choices[i].isCorrect;
+      // disable choices after selection is made
+      for (let node of choiceBtns) {
+        node.disabled = true;
+      }
 
-      // when choice button is clicked
-      c.addEventListener("click", function () {
-        if (isCorrect) {
-          score++;
-          result.style.display = "block";
-          correctTxt.style.display = "block";
-          wrongTxt.style.display = "none";
-          console.log("correct!");
-        } else {
-          result.style.display = "block";
-          correctTxt.style.display = "none";
-          wrongTxt.style.display = "block";
-          console.log("wrong...you get zero points");
-        }
-        
-        // disable choices after selection is made
-        for (let node of choiceBtns) {
-            node.disabled = true;
-        }
-
-        // show next button
-        nextBtn.style.display = "block";
-      });
-    }
+      // show next button
+      displayToggle.show(nextBtn);
+    });
   }
 }
