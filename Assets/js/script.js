ee85732124f95826390b5b5edcf17b20122c9deb
Patrick Richardson Wright <wrightmight@gmail.com>
2022-06-20 20:38:52 -0700

    create backup of index file

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,56 +1,4 @@
-// array of objects with all questions and answers
-const QuizContent = [
-  {
-    id: 1,
-    question: "Inside which HTML element do we put our JS code?",
-    choices: [
-      { text: "<js>", isCorrect: false },
-      { text: "<script>", isCorrect: true },
-      { text: "<javascript>", isCorrect: false },
-      { text: "<scripts>", isCorrect: false },
-    ],
-  },
-  {
-    id: 2,
-    question: "How do you write 'Hello World' in an alert box?",
-    choices: [
-      { text: "alertBox('Hello World')", isCorrect: false },
-      { text: "msgBox('Hello World')", isCorrect: false },
-      { text: "alert('Hello World')", isCorrect: true },
-      { text: "msg('Hello World')", isCorrect: false },
-    ],
-  },
-  {
-    id: 3,
-    question: "What is the correct way to write a JavaScript array?",
-    choices: [
-      { text: 'var colors = ["red", "green", "blue"]', isCorrect: true },
-      { text: 'var colors = (1:"red", 2:"green", 3:"blue")', isCorrect: false },
-      { text: 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', isCorrect: false },
-      { text: 'var colors = "red", "green", "blue"', isCorrect: false },
-    ],
-  },
-  {
-    id: 4,
-    question: "Which event occurs when the user clicks on an HTML element?",
-    choices: [
-      { text: "onmouseover", isCorrect: false },
-      { text: "onclick", isCorrect: true },
-      { text: "onchange", isCorrect: false },
-      { text: "onmouseclick", isCorrect: false },
-    ],
-  },
-  {
-    id: 5,
-    question: 'How do write an IF statement testing that "i" is NOT equal to 5?',
-    choices: [
-      { text: "if (i <> 5)", isCorrect: false },
-      { text: "if i <> 5", isCorrect: false },
-      { text: "if i =! 5 then", isCorrect: false },
-      { text: "if (i != 5)", isCorrect: true }
-    ],
-  },
-];
+
 
 const startSection = document.getElementById("start");
 const quizSection = document.getElementById("quiz");
@@ -265,3 +213,65 @@ function quiz(id) {
     });
   }
 }
+
+// array of objects with all questions and answers
+const QuizContent = [
+  {
+    id: 1,
+    question: "Inside which HTML element do we put our JS code?",
+    choices: [
+      { text: "<js>", isCorrect: false },
+      { text: "<script>", isCorrect: true },
+      { text: "<javascript>", isCorrect: false },
+      { text: "<scripts>", isCorrect: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "How do you write 'Hello World' in an alert box?",
+    choices: [
+      { text: "alertBox('Hello World')", isCorrect: false },
+      { text: "msgBox('Hello World')", isCorrect: false },
+      { text: "alert('Hello World')", isCorrect: true },
+      { text: "msg('Hello World')", isCorrect: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "What is the correct way to write a JavaScript array?",
+    choices: [
+      { text: 'var colors = ["red", "green", "blue"]', isCorrect: true },
+      { text: 'var colors = (1:"red", 2:"green", 3:"blue")', isCorrect: false },
+      { text: 'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', isCorrect: false },
+      { text: 'var colors = "red", "green", "blue"', isCorrect: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "Which event occurs when the user clicks on an HTML element?",
+    choices: [
+      { text: "onmouseover", isCorrect: false },
+      { text: "onclick", isCorrect: true },
+      { text: "onchange", isCorrect: false },
+      { text: "onmouseclick", isCorrect: false },
+    ],
+  },
+  {
+    id: 5,
+    question: 'How do write an IF statement testing that "i" is NOT equal to 5?',
+    choices: [
+      { text: "if (i <> 5)", isCorrect: false },
+      { text: "if i <> 5", isCorrect: false },
+      { text: "if i =! 5 then", isCorrect: false },
+      { text: "if (i != 5)", isCorrect: true }
+    ],
+  },
+];
+
+  // Pagination
+  const nextButton = document.getElementById("next");
+
+    // Btn clicks
+    nextButton.addEventListener("click", showNextSlide);
+    submitButton.addEventListener("click", showResults);
+  
